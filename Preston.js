c66aaa3eb568cbb1b081fee1e423d8e6f5a709bb
Preston Moore <prestonmoore@me.com>
2015-06-18 14:23:39 -0400

    finishing touches

--- Preston.js
+++ Preston.js
@@ -1,14 +1,18 @@
 
 function Samuri(weapon) {
 	this.weapon = weapon;
+	this.stamina = 10;
 	this.attack = function() {
 		console.log('attack!!')
 	
 }};
 
+var Jin = new Samuri('spear')
+
 function MountedSamuri() {
-	console.log('on a horse');
+	return true;
 }
 
 MountedSamuri.prototype = new Samuri('sword');
 
+var Atilla = new MountedSamuri()
